2b0c16ca703c1c087bd3f544b93a16484ea0759e
davidblancolopez <daviddbl94@gmail.com>
2017-02-08 17:51:31 +0100

    CAMBIOS

--- src/exercici/pkg2/m9/uf3/Exercici2M9UF3.java
+++ src/exercici/pkg2/m9/uf3/Exercici2M9UF3.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
 
 public class Exercici2M9UF3 {
 
@@ -34,7 +36,7 @@ public class Exercici2M9UF3 {
     public static void llegirURL() throws IOException {
         try {
             URL url = new URL("http://www.elpais.es");
-            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()) );
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
             String linea;
             while ((linea = in.readLine()) != null) {
                 System.out.println(linea);
@@ -45,10 +47,40 @@ public class Exercici2M9UF3 {
             System.out.println("Error en la comunicación");
         }
     }
-    
-    
-    public void comunicacioURL(){
-        
+
+    public void comunicacioURL() throws MalformedURLException {
+        Scanner lector = new Scanner(System.in);
+        System.out.println("Introdueix la URL: ");
+        String url = lector.next();
+
+        try {
+            URL url2 = new URL(url);
+
+            URLConnection conexion
+                    = url2.openConnection();
+            conexion.setDoOutput(true);
+            conexion.connect();
+            //Lectura i mostra de les capçaleres
+            int i = 1;
+
+            while (conexion.getHeaderFieldKey(i) != null) {
+                System.out.println(conexion.getHeaderFieldKey(i) + 
+                ":" + conexion.getHeaderField(i));
+                i++;
+            }
+
+            //Lectura i mostra del contingut
+            BufferedReader in = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+            String s
+                    = in.readLine();
+            while (s != null) {
+                System.out.println(s);
+                s
+                        = in.readLine();
+            }
+        } catch (Exception e) {
+            System.out.println("Fallo");
+        }
     }
 
 }
